699f712c974d086d384c45ae84254c4e3a5f570e
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-03-15 20:33:29 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: France_King,
+    default: Brasho_Kish,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function FLASH_MD_PAIR_CODE() {
+        async function LEGACY_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_France_King = France_King({
+            let Pair_Code_By_Brasho_Kish = France_King({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,16 +35,16 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_France_King.authState.creds.registered) {
+             if(!Pair_Code_By_Brasho_Kish.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_France_King.requestPairingCode(num)
+                            const code = await Pair_Code_By_Brasho_Kish.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_France_King.ev.on('creds.update', saveCreds)
-            Pair_Code_By_France_King.ev.on("connection.update", async (s) => {
+            Pair_Code_By_Brasho_Kish.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Brasho_Kish.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -56,35 +56,33 @@ router.get('/', async (req, res) => {
                let b64data = Buffer.from(data).toString('base64');
                let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: '' + b64data });
 
-               let FLASH_MD_TEXT = `
-*_Pair Code for FLASH-MD Connected_*
-*_Made With 🤍_*
-*By France King*
+               let LEGACY_MD_TEXT = `
+*_Pair Code for LEGACY-MD Connected_*
+*_Made With 𓅃_*
+*By Brasho Kish*
 ______________________________________
 ╔════◇
-║ *『 THANKS FOR CHOOSING FLASH-MD 』*
+║ *『 THANKS FOR CHOOSING LEGACY-MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╰═════════════⊷
 ╔═════◇
 ║  『••• FOR HELP VISIT•••』
-║❒ *Instagram:* _https://instagram.com/france.king1_
-║❒ *Owner:* _https://wa.me/254757835036_
-║❒ *Repo:* _https://github.com/franceking1/Flash-Md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/CtBGmUkwXiA6kGYAyU5MaX_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaTbb3p84Om9LRX1jg0P_
+║❒ *Instagram:* _https://instagram.com/Brashokish_
+║❒ *Owner:* _https://wa.me/254735905972_
+║❒ *Repo:* _https://github.com/Brashokish/Legacy-Md_
 ╰═════════════⊷ 
 _____________________________________
 
 _Don't Forget To Give a Star To My Repo_`
- await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
+ await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id,{text:LEGACY_MD_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_France_King.ws.close();
+        await Pair_Code_By_Brasho_Kish.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    FLASH_MD_PAIR_CODE();
+                    LEGACY_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -95,6 +93,6 @@ _Don't Forget To Give a Star To My Repo_`
          }
         }
     }
-    return await FLASH_MD_PAIR_CODE()
+    return await LEGACY_MD_PAIR_CODE()
 });
 module.exports = router
