5233bb4c16c1f150917711b86edd4561fb1ea0ce
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-15 04:28:32 +0300

    Flash Md

--- pair.js
+++ pair.js
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_France_King.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: '' + b64data });
 
                let FLASH_MD_TEXT = `
 *_Pair Code Connected by France King_*
