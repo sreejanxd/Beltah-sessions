cec28bdafa619669413923174bf0a42fa9a9160a
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-15 03:36:50 +0300

    Flash Md

--- qr.js
+++ qr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Gifted_Tech,
+	default: France_King,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function GIFTED_MD_QR_CODE() {
+	async function FLASH_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Gifted_Tech = Gifted_Tech({
+			let Qr_Code_By_France_King = France_King({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+			Qr_Code_By_France_King.ev.on('creds.update', saveCreds)
+			Qr_Code_By_France_Kingr.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,35 +56,49 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Gifted_Tech.sendMessage(Qr_Code_By_Gifted_Tech.user.id, { text: 'GIFTED-MD;;;' + b64data });
+				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: '' + b64data });
 	
-				   let GIFTED_MD_TEXT = `
-*_Qr Code By Gifted Tech_*
+				   let FLASH_MD_TEXT = `
+*_Qr Code By France King_*
 *_Made With 🤍_*
+______________________________________
+╔════◇
+║ *『 WOW YOU CHOOSEN FLASH-MD 』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚══════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Instagram:* _https://instagram.com/france.king1_
+║❒ *Owner:* _https://wa.me/254757835036_
+║❒ *Repo:* _https://github.com/franceking1/Flash-Md_
+║❒ *WaGroup:* _https://chat.whatsapp.com/CtBGmUkwXiA6kGYAyU5MaX_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaTbb3p84Om9LRX1jg0P_
+╚══════════════════════╝ 
+_____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Gifted_Tech.sendMessage(Qr_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Gifted_Tech.ws.close();
+					await Qr_Code_By_France_King.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					GIFTED_MD_QR_CODE();
+					FLASH_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
 			if (!res.headersSent) {
 				await res.json({
-					code: "Service Unavailable"
+					code: "Service is Currently Unavailable"
 				});
 			}
 			console.log(err);
 			await removeFile("temp/" + id);
 		}
 	}
-	return await GIFTED_MD_QR_CODE()
+	return await FLASH_MD_QR_CODE()
 });
 module.exports = router
